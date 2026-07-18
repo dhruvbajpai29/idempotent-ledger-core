28906ef33768306f90f26b688950b9b4add65b9d
Dhruv Bajpai <123343459+dhruvbajpai29@users.noreply.github.com>
2026-07-18 15:51:00 +0530

    Create LedgerCoreApplication.java

---
+++ src/main/java/com/ledger/core/LedgerCoreApplication.java
@@ -0,0 +1,12 @@
+package com.ledger.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerCoreApplication.class, args);
+    }
+}
